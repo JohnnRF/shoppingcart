b28a28021724ecb7d981a6c1f8f07cf2d375862d
johnnramirez <johnnramirezf99@gmail.com>
2024-06-07 16:02:08 -0500

    Actualización a la nueva sintaxis de Angular

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+
 
 import { ProductComponent } from "@products/components/product/product.component";
 import { HeaderComponent } from '@shared/components/header/header.component';
@@ -8,22 +8,43 @@ import { Product } from '@shared/models/product.model';
 
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export default class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
+
+
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+
+  @Input() category_id? :string;
 
   ngOnInit(){
-    this.productService.getProducts()
+    this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    this.getProducts()
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts(){
+    this.productService.getProducts(this.category_id)
     .subscribe({
       next: (products)=>{
         this.products.set(products);
@@ -34,7 +55,16 @@ export class ListComponent {
     })
   }
 
-  addToCart(product: Product){
-    this.cartService.addToCart(product);
+  private getCategories(){
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data)=>{
+        this.categories.set(data);
+      },
+      error: ()=>{
+
+      }
+    })
   }
+
 }
